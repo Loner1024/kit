9fc0202bd4ca3177d15ccd470caa441f04674460
Loner1024 <xth12138@gmail.com>
2021-09-11 11:36:45 +0800

    feat:kratos logger impl

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Loner1024/kit
 
 go 1.16
+
+require (
+	github.com/go-kratos/kratos/v2 v2.0.5
+	github.com/rs/zerolog v1.25.0
+)
---
+++ kratos/logger/logger.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	klog "github.com/go-kratos/kratos/v2/log"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type logger zerolog.Logger
+
+// Log implement Kratos log interface
+func (l logger) Log(level klog.Level, keyvals ...interface{}) error {
+	switch level {
+	case klog.LevelDebug:
+		log.Debug().Interface("", keyvals).Send()
+	case klog.LevelInfo:
+		log.Info().Interface("", keyvals).Send()
+	case klog.LevelWarn:
+		log.Warn().Msgf("%v", keyvals...)
+	case klog.LevelError:
+		log.Error().Msgf("%v", keyvals...)
+	}
+	return nil
+}
